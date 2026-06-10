e1c25d4c97b51e4456df72cb658628b574f30b56
Perez Facundo <perezfaq98@gmail.com>
2026-06-09 21:49:51 -0300

    09.06

--- 11_classes.py
+++ 11_classes.py
@@ -23,9 +23,12 @@ class Animal():
 # 4. Añade a la clase "Car" un método llamado "accelerate" que aumente la velocidad en 10 unidades. Añade también un método "brake" que reduzca la velocidad en 10 unidades. Asegúrate de que la velocidad no sea negativa.
 class Car():
     def __init__(self, brand, model):
-        self.__brand = __brand
-        self.__model = __model
+        self.__brand = brand
+        self.__model = model
         self.__speed = 0
+
+    def __str__(self) -> str:
+        return f"{self.__brand} - {self.__model}"
 
     def get_brand(self):
         return self.__brand
@@ -44,8 +47,8 @@ class Car():
 # 5. Crea una clase "Book" que tenga propiedades como "title" (público) y "author" (privado). Añade un método para obtener el autor y otro para cambiar el título del libro.
 class Book():
     def __init__(self, title, author):
-        self.__title = __title
-        self.__author = __author
+        self.__title = title
+        self.__author = author
 
     def get_author(self):
         return self.__author
@@ -93,19 +96,65 @@ class BankAccount():
 
 # 8. Crea una clase "Point" que represente un punto en el espacio 2D con coordenadas "x" e "y". Añade un método que calcule la distancia entre dos puntos.
 class Point():
+    
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
 
-    def distance_to(px):
-        d = 0
+    def get_x(self):
+        return self.__x
 
-        return d
+    def get_y(self):
+        return self.__y
+
+    def distance_to(self, b2):
+        import math
+        dx = b2.get_x() - self.__x
+        dy = b2.get_y() - self.__y
+        return round(math.sqrt(dx ** 2 + dy ** 2), 2)
 
 p1 = Point(10, 20)
 p2 = Point(30, 60)
-
+print(p1.distance_to(p2))
 
 # 9. Crea una clase "Employee" que tenga propiedades como "name", "hourly_wage" (pago por hora) y "hours_worked". Añade un método que calcule el pago total basado en las horas trabajadas y el salario por hora.
+class Employee():
+    def __init__(self, name, hourly_wage, hours_worked):
+        self.__name = name
+        self.__hourly_wage = hourly_wage
+        self.__hours_worked = hours_worked
+
+    def calc_liq(self):
+        return self.__hourly_wage * self.__hours_worked
+
+emp1 = Employee("Juan Perez", 52, 3500)
+print(emp1.calc_liq())
+emp2 = Employee("Walter Urquiza", 40, 3500)
+print(emp2.calc_liq())
 
 # 10. Crea una clase "Store" que tenga una propiedad "inventory" (una lista de productos). Añade un método para agregar un producto al inventario y otro para mostrar todos los productos disponibles.
+class Store():
+    def __init__(self, name):
+        self.__name = name
+        self.__inventory = []
+    
+    def add_car(self, car):
+        self.__inventory.append(car)
+    
+    def add_list_cars(self, *cars):
+        for each in cars:
+            self.__inventory.append(each)
+    
+    def get_inventory(self):
+        return self.__inventory
+
+st = Store("Zarate 1550")
+car1 = Car("Ferrari", "458 Spider")
+car2 = Car("BMW", "320i")
+car3 = Car("Nissan", "Silvia")
+car4 = Car("Hyundai", "Tiburon")
+st.add_list_cars(car1, car2, car3, car4)
+lista = st.get_inventory()
+
+for car in lista:
+    print(car)
